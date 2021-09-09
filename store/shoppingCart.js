7121892ea743f7a1ac13af411991b6133ae407e7
AtsuyaSakamoto <sakamo.070501@gmail.com>
2021-09-09 11:22:18 +0900

    新規カートの作成、既存カートへの追加機能を実装

--- store/shoppingCart.js
+++ store/shoppingCart.js
@@ -1,44 +1,28 @@
-// import axios from 'axios'
-
 export const state = () => ({
-  cart: [],
+  cart: null,
 })
 
 export const getters = {}
 
 export const mutations = {
   newCart(state, item) {
-    state.cart.push(item)
-    console.log('newCart')
+    // state.cart = []
+    state.cart = item
   },
   addCart(state, item) {
-    state.cart.push(item)
-    console.log('addCart')
+    state.cart = item
   },
 }
 
 export const actions = {
   newCart({ commit }, item) {
-    // axios
-    //   .post('http://localhost:3000/api/new-cart', { body: 'aaa' })
-    //   .then((res) => {
-    //     console.log('サーバーからのレスポンス')
-    //     console.log(res)
-    //     commit('newCart', item)
-    //   })
-
-    // this.$axios.$post('/', item).then((res) => {
-    //   console.log(item)
-    //   // console.log(res.$post)
-    //   commit('newCart', item)
-    // })
-    this.$axios.$post('/api/cart-cretate', { foo: item }).then((res) => {
-      // console.log(res)
-      // console.log(res.$post)
+    this.$axios.$post('/api/cart-cretate', { item }).then((res) => {
       commit('newCart', item)
     })
   },
   addCart({ commit }, item) {
-    commit('addCart', item)
+    this.$axios.$post('/api/cart-update', { item }).then((res) => {
+      commit('addCart', item)
+    })
   },
 }
