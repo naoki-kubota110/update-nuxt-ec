336dfc57f9d9eefd05907ca9e40c413111f84987
naoki-kubota110 <naoki.kubota@rakus-partners.co.jp>
2021-10-04 14:09:12 +0900

    change axios url

--- nuxt.config.js
+++ nuxt.config.js
@@ -67,7 +67,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // baseURL: 'http://localhost:3000/',
-    baseURL: 'https://nuxt-ec-pej3mzrly-naoki-kubota110.vercel.app/',
+    baseURL: 'https://nuxt-ec-pej3mzrly-naoki-kubota110.vercel.app/api/',
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
